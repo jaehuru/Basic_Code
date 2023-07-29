3f1ad648c0b125e8563081c34bb8e231fc5462df
jaehuru <kimbs0516@gamil.com>
2023-07-29 21:47:49 +0900

    July 29th

---
+++ _005_Control/_02_switch.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+void main() {
+	char ch;
+
+	printf("scan operator (+, -, *, /, %) >> ");
+	scanf_s("%c", &ch);
+
+	switch (ch)
+	{
+	case '+':
+		printf("+ operator\n");
+		break;
+	case '-':
+		printf("- operator\n");
+		break;
+	case '*':
+		printf("* operator\n");
+		break;
+	case '/':
+		printf("/ operator\n");
+		break;
+	case '%':
+		printf("% operator\n");
+		break;
+	default:
+		printf("nat an operator\n");
+		break;
+	}
+}
---
+++ _006_Function/_02_local_global.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int num = 100; //전역변수
+
+int getNum() {
+	int num = 10;
+	return num;
+}
+
+int getGNum() {
+	return num;
+}
+
+void main() {
+	int ret = getNum();
+	printf("%d\n", ret);
+	ret = getGNum();
+	printf("%d\n", ret);
+	printf("%d\n", num);
+}
